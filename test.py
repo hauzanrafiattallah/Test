cfe06f664dc711efe7dd6be925824e6fdabe2796
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-08-27 09:49:10 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import os, random
+
+for i in range(50):
+  d = str(i) + ' days ago'
+  rand = random.randrange(1, 12)
+  with open('file.txt', 'a') as file:
+      file.write(d + '\n' )
+  os.system('git add file.txt')
+  os.system('git commit --date=" 2024-' + str(rand) + '-'+d+'" -m 1')
+  
+os.system('git push -u origin main -f')
